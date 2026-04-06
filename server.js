41e2a3df720c3b15f5b5d366c30729b4538dbc91
Ayush <ayushbhardwaj1334@gmail.com>
2026-04-06 14:13:36 +0530

    fix host binding

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(PORT , ()=>{
-      console.log(`Example app listening on port ${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Example app listening on port ${PORT}`);
 });
